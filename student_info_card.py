62b5fa1ff2f7b0f7b26c6c44c424e4a33a421230
gowrishankarc2899 <gowrishankarc2899@gmail.com>
2025-04-08 18:26:17 +0530

    Add files via upload
    Day 1 Mini Project - Student Info Card

---
+++ student_info_card.py
@@ -0,0 +1,11 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+height = float(input("Enter your height:"))
+is_student_input = input("Are you a student? (True/False):")
+
+is_student = is_student_input == "True"
+
+print ("name:",name)
+print("age:",age)
+print("height:",height)
+print("is_student:",is_student)
